2ed92f7be2f547d3cdae93de8f20f718fb2918a6
Sergio Pérez <sergio16x@gmail.com>
2018-06-11 18:38:31 +0200

    R1

---
+++ ConsoleApp/ConsoleApp/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Class1
+    {
+        //Class1
+        public int suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int resta(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
