ca22e6b296bbd250ddbb1268fb72ab6d14a9fd41
Sergio Pérez <sergio16x@gmail.com>
2018-06-11 18:43:01 +0200

    second commit

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            //Program
+            class1 s = new class1();
+            class2 r = new class2();
+            Console.WriteLine("La suma de 3 + 3 es " + s.suma(3, 3));
+            Console.WriteLine("La resta de 4 - 3 es " + s.resta(4, 3));
+            Console.WriteLine("La multiplicación de 4 x 5 es " + s.multi(4, 5));
+            Console.WriteLine("La división de 6 / 3 es " + s.divi(6, 3));
         }
     }
 }
